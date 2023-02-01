9c857c8a7be52c98134a0940a1342a0bc645f22e
Brandon Garcia <grandonleebarcia@gmail.com>
2023-01-31 19:21:29 -0800

    configure app.js to add gsap animation

--- app.js
+++ app.js
@@ -21,3 +21,33 @@ links.forEach((link) => {
     });
   });
 });
+
+//Cards
+
+const cards = document.querySelectorAll('.card');
+
+cards.forEach((card, index) => {
+  card.addEventListener('click', () => {
+    //Get State
+
+    const state = Flip.getState(cards);
+
+    //Add the active class to clicked on and add inactive to the others
+    const isCardActive = card.classList.contains('active');
+    cards.forEach((otherCard, otherIdx) => {
+      otherCard.classList.remove('active');
+      otherCard.classList.remove('is-inactive');
+      if (!isCardActive && index !== otherIdx) {
+        otherCard.classList.add('is-inactive');
+      }
+    });
+
+    if (!isCardActive) card.classList.add('active');
+
+    Flip.from(state, {
+      duration: 1,
+      ease: 'expo.out',
+      absolute: true,
+    });
+  });
+});
